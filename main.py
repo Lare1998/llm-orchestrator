6a9dc7a6da89bd7716055a47d51e68137aa590d1
Alexander Reed <zetwitatelster@gmx.net>
2023-01-02 12:24:19 -0500

    feat: Develop distributed vision pipeline orchestration

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from llm_orchestrator.agent import LLMAgent, Orchestrator
+
+if __name__ == "__main__":
+    print("Starting LLM Orchestrator application...")
+
+    # Initialize agents
+    data_analyst = LLMAgent("DataAnalyst", "analyzes data", ["data_analysis", "report_generation"], model="gpt-4")
+    content_writer = LLMAgent("ContentWriter", "generates content", ["text_generation", "summarization"], model="gpt-3.5-turbo")
+    
+    # Create an orchestrator instance
+    orchestrator = Orchestrator(agents=[data_analyst, content_writer])
+
+    # Define a complex workflow
+    goal = "Develop a comprehensive marketing strategy for a new AI product."
+    steps = [
+        {"description": "Research market trends and competitor analysis", "required_capabilities": ["data_analysis"]},
+        {"description": "Generate initial product messaging and value propositions", "required_capabilities": ["text_generation"]},
+        {"description": "Summarize research findings for executive review", "required_capabilities": ["summarization"]},
+        {"description": "Draft marketing campaign outlines", "required_capabilities": ["text_generation"]},
+        {"description": "Analyze campaign performance metrics", "required_capabilities": ["data_analysis"]},
+        {"description": "Generate final report on marketing strategy", "required_capabilities": ["report_generation"]},
+    ]
+
+    # Run the workflow
+    results = orchestrator.run_workflow(goal, steps)
+    print("\nWorkflow execution complete. Results:")
+    for res in results["steps_results"]:
+        print(f"- Task {res["task_id"][:8]}...: {res["status"]} - {res["output"]}")
+
+    print("\nLLM Orchestrator application finished.")
---
+++ src/llm_orchestrator/agent.py
@@ -0,0 +1,129 @@
+
+import uuid
+from typing import Dict, Any, List, Optional
+
+class LLMAgent:
+    """Represents an individual LLM agent with a specific role and capabilities."""
+    def __init__(self, name: str, role: str, capabilities: List[str], model: str = "gpt-4"):
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.role = role
+        self.capabilities = capabilities
+        self.model = model
+        self.memory: List[Dict[str, Any]] = []
+
+    def __str__(self):
+        return f"Agent(name={self.name}, role={self.role}, model={self.model})"
+
+    def add_to_memory(self, entry: Dict[str, Any]):
+        """Adds an entry to the agent's memory."""
+        self.memory.append(entry)
+
+    def get_memory(self, limit: int = 5) -> List[Dict[str, Any]]:
+        """Retrieves recent memory entries."""
+        return self.memory[-limit:]
+
+    def act(self, task: Dict[str, Any]) -> Dict[str, Any]:
+        """Simulates the agent performing an action based on a task."""
+        print(f"Agent {self.name} ({self.role}) is processing task: {task['description']}")
+        # In a real scenario, this would involve calling the LLM API
+        # and processing its response based on capabilities.
+        response = {
+            "agent_id": self.id,
+            "task_id": task["id"],
+            "output": f"Completed: {task['description']}",
+            "status": "completed",
+            "timestamp": datetime.now().isoformat()
+        }
+        self.add_to_memory({"task": task, "response": response})
+        return response
+
+class TaskManager:
+    """Manages the creation, assignment, and status of tasks for LLM agents."""
+    def __init__(self):
+        self.tasks: Dict[str, Dict[str, Any]] = {}
+        self.task_queue: List[Dict[str, Any]] = []
+
+    def create_task(self, description: str, priority: int = 1, metadata: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        task_id = str(uuid.uuid4())
+        task = {
+            "id": task_id,
+            "description": description,
+            "priority": priority,
+            "status": "pending",
+            "assigned_agent": None,
+            "metadata": metadata or {},
+            "created_at": datetime.now().isoformat()
+        }
+        self.tasks[task_id] = task
+        self.task_queue.append(task)
+        self.task_queue.sort(key=lambda t: t['priority'], reverse=True)
+        return task
+
+    def assign_task(self, task_id: str, agent: LLMAgent) -> bool:
+        if task_id in self.tasks and self.tasks[task_id]["status"] == "pending":
+            self.tasks[task_id]["assigned_agent"] = agent.id
+            self.tasks[task_id]["status"] = "assigned"
+            return True
+        return False
+
+    def update_task_status(self, task_id: str, status: str, output: Optional[Any] = None) -> bool:
+        if task_id in self.tasks:
+            self.tasks[task_id]["status"] = status
+            if output: self.tasks[task_id]["output"] = output
+            return True
+        return False
+
+class Orchestrator:
+    """Coordinates multiple LLM agents to achieve complex goals."""
+    def __init__(self, agents: List[LLMAgent]):
+        self.agents = {agent.id: agent for agent in agents}
+        self.task_manager = TaskManager()
+
+    def add_agent(self, agent: LLMAgent):
+        self.agents[agent.id] = agent
+
+    def remove_agent(self, agent_id: str):
+        if agent_id in self.agents: del self.agents[agent_id]
+
+    def run_workflow(self, goal: str, steps: List[Dict[str, Any]]) -> Dict[str, Any]:
+        print(f"Orchestrating workflow for goal: {goal}")
+        workflow_results = {"goal": goal, "steps_results": []}
+
+        for step in steps:
+            task = self.task_manager.create_task(step["description"], priority=step.get("priority", 1))
+            assigned = False
+            for agent_id, agent in self.agents.items():
+                if any(cap in agent.capabilities for cap in step["required_capabilities"]):
+                    self.task_manager.assign_task(task["id"], agent)
+                    result = agent.act(task)
+                    self.task_manager.update_task_status(task["id"], result["status"], result["output"])
+                    workflow_results["steps_results"].append(result)
+                    assigned = True
+                    break
+            if not assigned:
+                print(f"No agent found for task: {task['description']}")
+                self.task_manager.update_task_status(task["id"], "failed", "No suitable agent")
+                workflow_results["steps_results"].append({"task_id": task["id"], "status": "failed", "output": "No suitable agent"})
+        
+        print(f"Workflow for goal '{goal}' completed.")
+        return workflow_results
+
+if __name__ == "__main__":
+    # Example Usage
+    analyst = LLMAgent("DataAnalyst", "analyzes data", ["data_analysis", "report_generation"])
+    writer = LLMAgent("ContentWriter", "generates content", ["text_generation", "summarization"])
+    
+    orchestrator = Orchestrator(agents=[analyst, writer])
+
+    workflow_goal = "Generate a market research report on AI trends."
+    workflow_steps = [
+        {"description": "Gather recent AI market data", "required_capabilities": ["data_analysis"]},
+        {"description": "Summarize key findings from data", "required_capabilities": ["summarization"]},
+        {"description": "Draft report sections", "required_capabilities": ["text_generation"]},
+        {"description": "Review and finalize report", "required_capabilities": ["report_generation"]},
+    ]
+
+    orchestrator.run_workflow(workflow_goal, workflow_steps)
+
+# Update on 2023-01-02 00:00:00
